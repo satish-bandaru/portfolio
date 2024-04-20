943ad99e47e9a1cd633e915a1516f5d7044ca125
Satish <>
2024-04-20 14:19:07 +0200

    Add Projects section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { About } from './components/About/About'
 import { Experience } from './components/Experience/Experience'
 import { Hero } from './components/Hero/Hero'
 import { Navbar } from './components/Navbar/Navbar'
+import { Projects } from './components/Projects/Projects'
 
 function App() {
   return <div className={styles.App}>
@@ -10,6 +11,7 @@ function App() {
     <Hero />
     <About />
     <Experience />
+    <Projects />
   </div>
 }
 
