e687ae51e895094c8189e9fbdd2428d2c7aa72f9
Satish Bandaru <satish.bandaru1234@gmail.com>
2024-04-20 13:48:22 +0200

    Add about section (#2)
    Co-authored-by: Satish <>

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import styles from './App.module.css'
+import { About } from './components/About/About'
 import { Hero } from './components/Hero/Hero'
 import { Navbar } from './components/Navbar/Navbar'
 
@@ -6,6 +7,7 @@ function App() {
   return <div className={styles.App}>
     <Navbar />
     <Hero />
+    <About />
   </div>
 }
 
