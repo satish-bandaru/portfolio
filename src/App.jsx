5bade3cb003ee73a78b8ee10679ef7fdb1df17a5
Satish Bandaru <satish.bandaru1234@gmail.com>
2024-04-20 14:28:02 +0200

    Add contact section (#5)
    Co-authored-by: Satish <>

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import styles from './App.module.css'
 import { About } from './components/About/About'
+import { Contact } from './components/Contact/Contact'
 import { Experience } from './components/Experience/Experience'
 import { Hero } from './components/Hero/Hero'
 import { Navbar } from './components/Navbar/Navbar'
@@ -12,6 +13,7 @@ function App() {
     <About />
     <Experience />
     <Projects />
+    <Contact />
   </div>
 }
 
